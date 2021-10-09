391c5eeff3b1b8c87c99310c8464b1a3e4bf01c9
Milas <84676044+killersparrow0@users.noreply.github.com>
2021-10-09 18:51:13 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -43,7 +43,7 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@elinmuskme
+@elonmuskme
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
